87f90eb99dc991dffd63977203be51820b29d539
Jason Yu <jasonyu.kr@gmail.com>
2025-09-24 22:30:14 +0900

    Revert "Remove title setting"
    This reverts commit fa2867bfae2f02bab4d82f36edb0c193bde2c5ec.

--- python/kitty_scrollback_nvim.py
+++ python/kitty_scrollback_nvim.py
@@ -201,6 +201,8 @@ def handle_result(args: List[str],
             'KITTY_SCROLLBACK_NVIM=true',
             '--type',
             'overlay',
+            '--title',
+            'kitty-scrollback.nvim',
         ) + env + cwd
 
         nvim_args = parse_nvim_args(args) + (
