9d0f38129ca589765942d8adb0977e567b0819aa
Jason Yu <jasonyu.kr@gmail.com>
2025-09-26 17:49:58 +0900

    Revert "nmap cr to quit"
    This reverts commit 715f473c74f8da2134e79767737d4935ee3ffdba.

--- lua/kitty-scrollback/keymaps.lua
+++ lua/kitty-scrollback/keymaps.lua
@@ -142,7 +142,6 @@ local function set_global_defaults()
   -- set_default({ 'v' }, '<c-cr>', plug.EXECUTE_VISUAL_CMD, {})
   set_default({ 'v' }, '<s-cr>', plug.PASTE_VISUAL_CMD, {})
 
-  set_default({ 'n' }, '<cr>', plug.CLOSE_OR_QUIT_ALL, {})
   vim.api.nvim_set_keymap('n', '<c-c>', ':lua CopyCurrentChunkOrWordAndQuit()<CR>', { noremap = true, silent = true })
   -- Ctrl-^ is bound to Ctrl-Enter in kitty config
   vim.api.nvim_set_keymap('n', '<c-^>', ':lua CopyCurrentChunkOrWordAndQuit()<CR>', { noremap = true, silent = true })
